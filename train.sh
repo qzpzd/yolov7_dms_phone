230cfd6e9aca4770ea38deb2915c740c0b518302
qzpzd <74693796+qzpzd@users.noreply.github.com>
2023-02-13 12:05:02 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,1 @@
+python train.py --workers 8 --device 0 --batch-size 32 --data data/coco.yaml --img 128 128 --cfg cfg/training/yolov7-tiny.yaml --weights '' --name yolov7-tiny --hyp data/hyp.scratch.tiny.yaml
